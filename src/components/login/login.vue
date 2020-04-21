8afdef5bc9c519c900a0444654f61cc92e9137e5
free_cheese <5252629+free_cheese@user.noreply.gitee.com>
2020-04-21 14:29:13 +0800

    图书管理系统

---
+++ src/components/login/login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="login" name="login">
+      <el-form :label-position="labelPosition" label-width="80px" :model="login">
+        <el-form-item label="用户名">
+          <el-input v-model="login.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="login.password"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "login",
+      data() {
+        return {
+          labelPosition: 'right',
+          formLabelAlign: {
+            name: '',
+            password: '',
+            type: ''
+          }
+        };
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
